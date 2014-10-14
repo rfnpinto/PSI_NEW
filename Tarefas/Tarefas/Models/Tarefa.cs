bddc8071e347bf3901feda1065bfb6967eea2a3a
rfnpinto <ricardo995.pinto@gmail.com>
2014-10-14 20:03:58 +0100

    Base de Dados embutida

---
+++ Tarefas/Tarefas/Models/Tarefa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tarefas.Models
+{
+    public class Tarefa
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "O Titulo deve ter menos de 50 caracteres;")]
+        public string Titulo { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "O tamanho maximo da nota é 250 caracteres;")]
+        public string Nota { get; set; }
+
+        [Required]
+        public bool Lido { get; set; } 
+    }
+}
---
+++ Tarefas/Tarefas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tarefas.Startup))]
+namespace Tarefas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
